58390033a460d0d6c6963a753f4bf5478dd44a39
Michael <michael.jordan17@gmail.com>
2015-07-22 10:23:51 -0300

    models

---
+++ app/src/main/java/br/com/jordan/cadeopenha/model/Penha.java
@@ -0,0 +1,58 @@
+package br.com.jordan.cadeopenha.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+/**
+ * Created by Michael on 18/07/2015.
+ */
+public class Penha implements Serializable {
+
+    @SerializedName("p")
+    @Expose
+    private int numero;
+
+    @SerializedName("py")
+    @Expose
+    private double latitude;
+
+    @SerializedName("px")
+    @Expose
+    private double longitude;
+
+    private double distancia;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(double distancia) {
+        this.distancia = distancia;
+    }
+}
---
+++ app/src/main/java/br/com/jordan/cadeopenha/model/Penhas.java
@@ -0,0 +1,37 @@
+package br.com.jordan.cadeopenha.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+import java.util.List;
+
+import br.com.jordan.cadeopenha.task.GoogleAddressTask;
+
+public class Penhas implements Serializable {
+
+    @SerializedName("hora")
+    @Expose
+    private String hora;
+
+    @SerializedName("vs")
+    @Expose
+    private List<Penha> listPenha;
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public List<Penha> getListPenha() {
+        return listPenha;
+    }
+
+    public void setListPenha(List<Penha> listPenha) {
+        this.listPenha = listPenha;
+    }
+
+}
